32f580e1a8c4b3ff3b44c621edfe77f08cf4dac8
alextanhongpin <alex@policypal.com>
2019-06-01 19:46:37 +0800

    chore: fix field name

--- timeseries/hash/main.go
+++ timeseries/hash/main.go
@@ -34,7 +34,7 @@ var defaultGranularities = map[string]Granularity{
 	// Stores 300 timestamps of 1 second each.
 	"1sec": Granularity{"1sec", 2 * Hour, Second, 5 * Minute},
 	// Stores 480 timestamps of 1 minute each.
-	"1min": Granularity{"1min", 7 * Day, Minute, 7 * Hour},
+	"1min": Granularity{"1min", 7 * Day, Minute, 8 * Hour},
 	// Stores 240 timestamps of 1 hour each.
 	"1hour": Granularity{"1hour", 60 * Day, Hour, 10 * Day},
 	// Stores a maximum of 30 timestamps of 1 day each.
@@ -56,18 +56,19 @@ func NewTimeSeries(client *redis.Client, namespace string) *TimeSeries {
 }
 
 func (t *TimeSeries) key(granularity Granularity, timestampInSeconds int64) string {
-	roundedTimestamp := t.roundTimestamp(granularity, timestampInSeconds)
+	roundedTimestamp := t.roundTimestamp(timestampInSeconds, granularity.Quantity)
 	return fmt.Sprintf("%s:%s:%d", t.namespace, granularity.Name, roundedTimestamp)
 }
 
-func (t *TimeSeries) roundTimestamp(granularity Granularity, timestampInSeconds int64) int64 {
-	return timestampInSeconds - (timestampInSeconds % granularity.Quantity)
+func (t *TimeSeries) roundTimestamp(timestampInSeconds, precision int64) int64 {
+	return timestampInSeconds - (timestampInSeconds % precision)
 }
 
 func (t *TimeSeries) Insert(timestampInSeconds int64) {
 	for _, granularity := range t.granularities {
 		key := t.key(granularity, timestampInSeconds)
-		field := strconv.FormatInt(t.roundTimestamp(granularity, timestampInSeconds), 10)
+		field := strconv.FormatInt(t.roundTimestamp(timestampInSeconds, granularity.Duration), 10)
+		fmt.Println(key, field)
 		t.client.HIncrBy(key, field, 1)
 		if granularity.TTL > 0 {
 			t.client.Expire(key, time.Duration(granularity.TTL)*time.Second)
@@ -81,22 +82,24 @@ func (t *TimeSeries) Fetch(granularityName string, startTimestamp, endTimestamp 
 	if !ok {
 		return nil, errors.New("granularity does not exist")
 	}
-	start := t.roundTimestamp(granularity, startTimestamp)
-	end := t.roundTimestamp(granularity, endTimestamp)
+	start := t.roundTimestamp(startTimestamp, granularity.Duration)
+	end := t.roundTimestamp(endTimestamp, granularity.Duration)
 	multi := t.client.Pipeline()
 
 	var result []*redis.StringCmd
 	for ts := start; ts <= end; ts += granularity.Duration {
 		key := t.key(granularity, ts)
-		field := strconv.FormatInt(t.roundTimestamp(granularity, ts), 10)
+		field := strconv.FormatInt(t.roundTimestamp(ts, granularity.Duration), 10)
 		result = append(result, multi.HGet(key, field))
 	}
-	_, err := multi.Exec()
+	res, err := multi.Exec()
 	if err != nil {
 		return nil, err
-	}
 
-	fmt.Println(result)
+	}
+	fmt.Println("exec res", res)
+
+	fmt.Println("multi", result)
 	output := make([]Series, len(result))
 	for i, res := range result {
 		val, err := res.Int64()
@@ -111,9 +114,9 @@ func (t *TimeSeries) Fetch(granularityName string, startTimestamp, endTimestamp 
 
 func NewClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
-		Addr:     "localhost:6379",
 	})
 }
 
